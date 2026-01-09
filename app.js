714286a22f17c1d128b95885e5cd39835c76704c
PogodaIgor <igor.pogoda2010@wp.pl>
2026-01-09 15:09:24 +0100

    a

---
+++ app.js
@@ -0,0 +1,43 @@
+var licznik = 1
+
+function generuj(id){
+
+    if(id=="jeden"){
+        document.getElementById("jeden").innerHTML = licznik
+    }
+
+    if(id=="dwa"){
+        document.getElementById("dwa").innerHTML = licznik
+    }
+
+    if(id=="trzy"){
+        document.getElementById("trzy").innerHTML = licznik
+    }
+
+    if(id=="cztery"){
+        document.getElementById("cztery").innerHTML = licznik
+    }
+
+    if(id=="piec"){
+        document.getElementById("piec").innerHTML = licznik
+    }
+
+    if(id=="szesc"){
+        document.getElementById("szesc").innerHTML = licznik
+    }
+
+    if(id=="siedem"){
+        document.getElementById("siedem").innerHTML = licznik
+    }
+    
+    if(id=="osiem"){
+        document.getElementById("osiem").innerHTML = licznik
+    }
+
+    if(id=="dziewiec"){
+        document.getElementById("dziewiec").innerHTML = licznik
+    }
+
+
+    licznik++
+}
