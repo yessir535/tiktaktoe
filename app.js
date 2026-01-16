d9212cecca468690f829ead5fc0f5225b3a330b2
PogodaIgor <igor.pogoda2010@wp.pl>
2026-01-16 15:01:04 +0100

    do domu

--- app.js
+++ app.js
@@ -4,38 +4,58 @@ function generuj(id){
 
     if(id=="jeden"){
         document.getElementById("jeden").innerHTML = licznik
+        document.getElementById("jeden").style.backgroundColor = "red"
     }
 
     if(id=="dwa"){
         document.getElementById("dwa").innerHTML = licznik
+        document.getElementById("dwa").style.backgroundColor = "purple"
     }
 
     if(id=="trzy"){
         document.getElementById("trzy").innerHTML = licznik
+        document.getElementById("trzy").style.backgroundColor = "green"
     }
 
     if(id=="cztery"){
         document.getElementById("cztery").innerHTML = licznik
+        document.getElementById("cztery").style.backgroundColor = "yellow"
     }
 
     if(id=="piec"){
         document.getElementById("piec").innerHTML = licznik
+        document.getElementById("piec").style.backgroundColor = "blue"
     }
 
     if(id=="szesc"){
         document.getElementById("szesc").innerHTML = licznik
+        document.getElementById("szesc").style.backgroundColor = "lightgreen"
     }
 
     if(id=="siedem"){
         document.getElementById("siedem").innerHTML = licznik
+        document.getElementById("siedem").style.backgroundColor = "gray"
     }
     
     if(id=="osiem"){
         document.getElementById("osiem").innerHTML = licznik
+        document.getElementById("osiem").style.backgroundColor = "lightgray"
     }
 
     if(id=="dziewiec"){
         document.getElementById("dziewiec").innerHTML = licznik
+        document.getElementById("dziewiec").style.backgroundColor = "lightblue"
+    }
+
+    if(id=="jeden"){
+        return
+    }
+
+    if(id=="dwa"){
+        document.getElementById("dwa").innerHTML = licznik + 1
+        if(id=="jeden"){
+            return
+        }
     }
 
 
