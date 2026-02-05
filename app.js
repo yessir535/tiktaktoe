be1a4b160699966f7fe4b3b4b6689c9a00ef1ec1
PogodaIgor <igor.pogoda2010@wp.pl>
2026-02-05 21:30:16 +0100

    g

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ function generuj(id) {
 
     licznik++;
   }
-    if(id=="jeden"){
-        document.getElementById("jeden").innerHTML = licznik
-        document.getElementById("jeden").style.backgroundColor = "red"
-    }
+
   if (id == "div2") {
     if (document.getElementById("div2").style.backgroundColor === "gray") {
       return;
@@ -25,58 +22,6 @@ function generuj(id) {
     document.getElementById("div2").style.backgroundColor = "gray";
 
     licznik++;
-    if(id=="dwa"){
-        document.getElementById("dwa").innerHTML = licznik
-        document.getElementById("dwa").style.backgroundColor = "purple"
-    }
-
-    if(id=="trzy"){
-        document.getElementById("trzy").innerHTML = licznik
-        document.getElementById("trzy").style.backgroundColor = "green"
-    }
-
-    if(id=="cztery"){
-        document.getElementById("cztery").innerHTML = licznik
-        document.getElementById("cztery").style.backgroundColor = "yellow"
-    }
-
-    if(id=="piec"){
-        document.getElementById("piec").innerHTML = licznik
-        document.getElementById("piec").style.backgroundColor = "blue"
-    }
-
-    if(id=="szesc"){
-        document.getElementById("szesc").innerHTML = licznik
-        document.getElementById("szesc").style.backgroundColor = "lightgreen"
-    }
-
-    if(id=="siedem"){
-        document.getElementById("siedem").innerHTML = licznik
-        document.getElementById("siedem").style.backgroundColor = "gray"
-    }
-    
-    if(id=="osiem"){
-        document.getElementById("osiem").innerHTML = licznik
-        document.getElementById("osiem").style.backgroundColor = "lightgray"
-    }
-
-    if(id=="dziewiec"){
-        document.getElementById("dziewiec").innerHTML = licznik
-        document.getElementById("dziewiec").style.backgroundColor = "lightblue"
-    }
-
-    if(id=="jeden"){
-        return
-    }
-
-    if(id=="dwa"){
-        document.getElementById("dwa").innerHTML = licznik + 1
-        if(id=="jeden"){
-            return
-        }
-    }
-  if (id == "dwa") {
-    document.getElementById("dwa").innerHTML = licznik;
   }
 
   if (id == "div3") {
